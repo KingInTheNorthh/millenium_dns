44bd902241a38c78899d87f3b44940b21ab010e4
KingInTheNorthh <goooogleboy23@gmail.com>
2026-06-13 15:50:17 +0000

    cleared build files

--- package/target/debug/deps/millenium_dns-15a71a7e142b3e75.d
+++
@@ -1,5 +0,0 @@
-/home/skywalker/Projects/millenium/package/target/debug/deps/millenium_dns-15a71a7e142b3e75.d: src/main.rs
-
-/home/skywalker/Projects/millenium/package/target/debug/deps/millenium_dns-15a71a7e142b3e75: src/main.rs
-
-src/main.rs:
--- package/target/debug/deps/millenium_dns-4d25a4ee46bfae3c.d
+++
@@ -1,5 +0,0 @@
-/home/skywalker/Projects/millenium/target/debug/deps/millenium_dns-4d25a4ee46bfae3c.d: src/main.rs
-
-/home/skywalker/Projects/millenium/target/debug/deps/millenium_dns-4d25a4ee46bfae3c: src/main.rs
-
-src/main.rs:
